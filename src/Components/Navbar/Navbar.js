e6d74d8eb9798fd7e408123c3a69f8e5ac1a564a
Pawan Singh <pmarsingh@gmail.com>
2021-08-18 00:02:44 +0530

    Styled components

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { NavContainer } from "./Navbar.styles";
+
+function Navbar() {
+  return <NavContainer>Hi</NavContainer>;
+}
+
+export default Navbar;
---
+++ src/Components/Navbar/Navbar.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 5em;
+  padding-inline: 3em;
+  background-color: #fafafa;
+`;
